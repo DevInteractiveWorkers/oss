144a6b8d139bbae457e2eb39d110de5bc64ef7db
Daniel Urdaneta <danielurn@hotmail.com>
2023-12-01 15:44:33 -0400

    responsive intro and sliding animation
    background and guard sliding inside view

--- app/components/banner/intro.tsx
+++ app/components/banner/intro.tsx
@@ -4,10 +4,11 @@ import React from 'react'
 
 const IntroBanner = () => {
   return (
-    <section className='relative w-full flex flex-col justify-end items-center min-h-screen intro-bg bg-no-repeat bg-cover bg-center md:bg-top'>
-        <div className='p-8 py-32 md:p-20 text-center'>
-            <h1 className='text-5xl md:text-8xl title font-bebas uppercase my-4'>Professional security services</h1>
-            <h2 className='text-3xl md:text-5xl font-normal'>that ensure the well-beign of private property across Oregon</h2>
+    <section className='relative w-full flex flex-col justify-end items-center min-h-[30vh] md:min-h-[1024px] home bg-no-repeat bg-cover slideBgLtoR overflow-hidden'>
+        <Image className='absolute bottom-0 right-0 z-0 slideGuardRtoL object-cover object-left-top h-full w-10/12 xl:w-7/12 max-h-full' src={'/intro.png'} alt='On service intro guard' width={1440} height={943}></Image>
+        <div className='relative z-10 p-8 pt-64 md:py-32 md:p-20 text-center'>
+            <h1 className='text-2xl md:text-8xl title font-bebas uppercase my-4'>Professional security services</h1>
+            <h2 className='text-xl md:text-5xl font-normal'>that ensure the well-beign of private property across Oregon</h2>
         </div>
     </section>
   )
--- app/service/page.tsx
+++ app/service/page.tsx
@@ -4,10 +4,10 @@ import ZebraPath from '../components/icons/zebra-path'
 const ServicePage = () => {
   return (
     <div className='relative overflow-hidden'>
-      <section className='bg-[#231F20] min-h-[60vh] flex flex-col justify-end items-center text-center md:pt-40'>
-        <div className='absolute -left-[20%] top-0 h-[633px] w-[633px] rounded-full mix-blend-lighten radialGradient slideLtoR'></div>
-        <div className='absolute right-0 -bottom-[70%] h-[633px] w-[633px] rounded-full mix-blend-lighten radialGradient slideRtoL'></div>
-        <h1 className='title text-8xl font-bebas uppercase '>Recuitment for <br /> on-service security</h1>
+      <section className='bg-[#231F20] flex flex-col justify-end items-center text-center pt-28 md:pt-40'>
+        <div className='absolute -left-[20%] top-0 aspect-square h-auto w-[40%] rounded-full mix-blend-lighten radialGradient slideLtoR'></div>
+        <div className='absolute -right-[10%] -bottom-[70%] aspect-square h-auto w-[40%] rounded-full mix-blend-lighten radialGradient slideRtoL'></div>
+        <h1 className='title text-6xl md:text-8xl font-bebas uppercase fadeInside'>Recuitment for <br /> on-service security</h1>
         <ZebraPath className=' mix-blend-soft-light'></ZebraPath>
       </section>
     </div>
